859feb42844928904f1d899153202afab8febf86
Andrew Monteiro <andrew.agoravai@gmail.com>
2022-03-03 00:54:27 -0300

    Feat: implement the reverse logic to find the biggest value in the array in index.js

--- index.js
+++ index.js
@@ -1,11 +1,17 @@
 const livros = require('./listaLivros');
 
 let maisBarato = 0;
+let maisCaro = 0;
 
 for (let atual = 0; atual < livros.length; atual++) {
     if (livros[atual].preco < livros[maisBarato].preco) {
         maisBarato = atual;
     }
+
+    if (livros[atual].preco > livros[maisCaro].preco) {
+        maisCaro = atual;
+    }
 }
 
 console.log(`O livro mais barato custa R$${livros[maisBarato].preco}.`);
+console.log(`O livro mais caro custa R$${livros[maisCaro].preco}.`);
