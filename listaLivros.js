f7300f0882390f5d47161d234d7e6ba5f4e55bdf
Andrew Monteiro <andrew.agoravai@gmail.com>
2022-03-03 00:49:34 -0300

    Feat: create an array of objects called 'livros' in listaLivros.js

---
+++ listaLivros.js
@@ -0,0 +1,22 @@
+const listaLivros = [
+    {
+        titulo: "JavaScript for Beginners",
+        preco: 25
+    }, {
+        titulo: "PHP for Beginners",
+        preco: 15
+    }, {
+        titulo: "Java for Beginners",
+        preco: 30
+    }, {
+        titulo: "Elixir for Beginners",
+        preco: 50
+    },
+    {
+        titulo: "Go for Goners",
+        preco: 45
+    }, {
+        titulo: "Python for Biologists",
+        preco: 20
+    }
+];
