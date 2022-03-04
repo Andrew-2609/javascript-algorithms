ca4d033435b45f0bf65130f3a8363e4e4eba7779
Andrew Monteiro <andrew.agoravai@gmail.com>
2022-03-03 22:32:50 -0300

    Feat: start the implementation of a binary search algorithm in busca.js

---
+++ secondCourse/aula-4/busca.js
@@ -0,0 +1,20 @@
+const listaLivros = require('./arrayOrdenado');
+
+function buscar(array, de, ate, valorBuscado) {
+    const meio = Math.floor((de + ate) / 2);
+    const atual = array[meio];
+
+    if (valorBuscado === atual.preco) {
+        return meio;
+    }
+
+    if (valorBuscado < atual.preco) {
+        return buscar(array, de, meio - 1, valorBuscado);
+    }
+
+    if (valorBuscado > atual.preco) {
+        return buscar(array, meio + 1, ate, valorBuscado);
+    }
+}
+
+console.log(buscar(listaLivros, 0, listaLivros.length - 1, 20));
